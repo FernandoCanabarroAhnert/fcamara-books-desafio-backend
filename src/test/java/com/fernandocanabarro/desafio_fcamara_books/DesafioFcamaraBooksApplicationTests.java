dd6496dfd3d05b6a57d2957ad7ad0fcb9ebeff0d
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2024-08-20 08:41:07 -0300

    :tada: init: Projeto Criado

---
+++ src/test/java/com/fernandocanabarro/desafio_fcamara_books/DesafioFcamaraBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.desafio_fcamara_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioFcamaraBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
